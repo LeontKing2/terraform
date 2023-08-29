88b9a6baa2ccc07ff4cff6d112efe6e5ef17b93a
Jared Baker <jared.baker@hashicorp.com>
2023-08-29 17:04:52 -0400

    backend/s3: add retry_mode argument

--- internal/backend/remote-state/s3/backend.go
+++ internal/backend/remote-state/s3/backend.go
@@ -167,6 +167,11 @@ func (b *Backend) ConfigSchema() *configschema.Block {
 				Optional:    true,
 				Description: "AWS profile name",
 			},
+			"retry_mode": {
+				Type:        cty.String,
+				Optional:    true,
+				Description: "Specifies how retries are attempted.",
+			},
 			"shared_config_files": {
 				Type:        cty.Set(cty.String),
 				Optional:    true,
@@ -805,6 +810,13 @@ func (b *Backend) Configure(obj cty.Value) tfdiags.Diagnostics {
 	}
 	if val, ok := boolAttrOk(obj, "use_dualstack_endpoint"); ok {
 		cfg.UseDualStackEndpoint = val
+	}
+
+	if v, ok := retrieveArgument(&diags,
+		newAttributeRetriever(obj, cty.GetAttrPath("retry_mode")),
+		newEnvvarRetriever("AWS_RETRY_MODE"),
+	); ok {
+		cfg.RetryMode = aws.RetryMode(v)
 	}
 
 	_ /* ctx */, awsConfig, cfgDiags := awsbase.GetAwsConfig(ctx, cfg)
